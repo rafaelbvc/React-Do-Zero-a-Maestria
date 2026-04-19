7d3cc566be3ac37edf132bceabb16744bb42ef6d
RafaelVendramini <rafaelbvc@hotmail.com>
2026-04-19 03:38:31 -0300

    Part 04 React CSS

---
+++ 04 - CSS REACT/cssreact/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Este paragrafo é do APP.jsx</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 04 - CSS REACT/cssreact/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS de componente</h1>
+      <p>Paragrafo do component</p>
+      <p className="my-comp-paragraph">Este também é do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;

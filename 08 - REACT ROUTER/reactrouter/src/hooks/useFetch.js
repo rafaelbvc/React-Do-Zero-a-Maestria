52c9f644cccc63591c81607859edbd997c707532
RafaelVendramini <rafaelbvc@hotmail.com>
2026-04-23 00:13:52 -0300

    08 REACT ROUTER

---
+++ 08 - REACT ROUTER/reactrouter/src/hooks/useFetch.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  const [error, setError] = useState(null);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "aplication/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod(method);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+
+        const json = await res.json();
+
+        setData(json);
+      } catch (error) {
+        console.log(error.message);
+        setError("Erro ao carregar dados!");
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [url, callFetch]);
+
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+
+    httpRequest();
+  }, [config, method, url]);
+
+  return { data, httpConfig, loading, error };
+};
---
+++ 08 - REACT ROUTER/reactrouter/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import "./Home.css";
+
+const Home = () => {
+  const url = "http://localhost:3001/products";
+
+  const { data: items, loading, error } = useFetch(url);
+
+  return (
+    <div>
+      <h1>Produtos</h1>
+      {error && <p>{error}</p>}
+      <ul className="products">
+        {items &&
+          items.map((item) => (
+            <li key={item.id}>
+              <h2>Nome: {item.name}</h2> <p>R$: {item.price}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
